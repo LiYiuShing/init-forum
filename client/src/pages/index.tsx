410b130358f559cda3e76596552121838693488e
Li Yiu Shing <18022213d@connect.polyu.hk>
2020-09-07 16:44:53 +0800

    Stack

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,20 +1,49 @@
-import { NavBar } from "../components/NavBar";
+import { Box, Heading, Link, Stack, Text, Flex, Button } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
-import { usePostsQuery } from "../generated/graphql";
-import { Layout } from "../components/Layout";
-import { Link } from "@chakra-ui/core";
 import NextLink from "next/link";
+import { Layout } from "../components/Layout";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data, fetching }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
+
+  if (!fetching && !data) {
+    return <div>You got query failed for some reason</div>;
+  }
+
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>create post</Link>
-      </NextLink>
-      <div>Hello World</div>
-      {!data ? null : data.posts.map((p) => <div key={p.id}>{p.title}</div>)}
+      <Flex align="center">
+        <Heading>ABC</Heading>
+        <NextLink href="/create-post">
+          <Link ml="auto">create post</Link>
+        </NextLink>
+      </Flex>
+      <br />
+      {!data && fetching ? (
+        <div>Loading ...</div>
+      ) : (
+        <Stack spacing={8}>
+          {data!.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data ? (
+        <Flex>
+          <Button isLoading={fetching} m="auto" my={4}>
+            Load More
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
